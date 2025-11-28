32878399b56314518f3b3b16718f63b964d9c164
jpluengo2 <jpluengo2@gmail.com>
2025-11-28 09:24:51 +0100

    Initial Commit

---
+++ PracticaFirebaseTests/PracticaFirebaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  PracticaFirebaseTests.swift
+//  PracticaFirebaseTests
+//
+//  Created by Mananas on 28/11/25.
+//
+
+import Testing
+@testable import PracticaFirebase
+
+struct PracticaFirebaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
